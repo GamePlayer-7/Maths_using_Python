5c03c4956214b849d38d30d0f109b1618686c74f
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2021-08-16 14:45:49 +0530

    Update Leap Year or not.py

--- Leap Year/Leap Year or not.py
+++ Leap Year/Leap Year or not.py
@@ -1,4 +1,4 @@
-year = int(input("Enter Year: "))
+year = int(input("Enter Year: "))   #User enters the year
 if year % 4 == 0 and year % 100 != 0:
     print(year, "is a Leap Year")
 elif year % 100 == 0:
